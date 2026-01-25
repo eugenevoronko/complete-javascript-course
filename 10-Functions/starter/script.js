1230e31e234ccad08e0f15cfbbf7a185be45c4c5
Eugene Voronko <voronkojr@gmail.com>
2026-01-25 23:23:41 +0200

    home commit

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const bookings = [];
+
+function createBooking(
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers,
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  return booking;
+}
+
+createBooking('LH123', undefined, 200);
+
+const greet = greeting => name => `${greeting}, ${name}!`;
+const greeterHey = greet('Hey');
+console.log(greeterHey('Jonas'));
+console.log(greeterHey('Steven'));
