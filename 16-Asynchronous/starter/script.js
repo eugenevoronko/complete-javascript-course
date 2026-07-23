a2837be781dff1ad9b26e61c572eb86944c1d40c
Eugene Voronko <voronkojr@gmail.com>
2026-07-23 09:43:43 +0300

    262 WIP

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -8,7 +8,7 @@ const countriesContainer = document.querySelector('.countries');
 
 function getCountryData(country) {
   const request = new XMLHttpRequest();
-  request.addEventListener('load', function () {
+  request.onload = function () {
     const data = JSON.parse(this.responseText).data.objects[0];
     console.log(data);
     const html = `
@@ -25,6 +25,78 @@ function getCountryData(country) {
 `;
     countriesContainer.insertAdjacentHTML('beforeend', html);
     countriesContainer.style.opacity = 1;
+  };
+  //   request.addEventListener('load', function () {
+  //     const data = JSON.parse(this.responseText).data.objects[0];
+  //     console.log(data);
+  //     const html = `
+  //     <article class="country">
+  //         <img class="country__img" src="${data.flag.url_svg}"/>
+  //         <div class="country__data">
+  //         <h3 class="country__name">${data.names.common}</h3>
+  //         <h4 class="country__region">${data.region}</h4>
+  //         <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)} millions</p>
+  //         <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+  //         <p class="country__row"><span>💰</span>${data.currencies[0].code}</p>
+  //         </div>
+  //     </article>
+  // `;
+  //     countriesContainer.insertAdjacentHTML('beforeend', html);
+  //     countriesContainer.style.opacity = 1;
+  //   });
+  request.open(
+    'GET',
+    `https://api.restcountries.com/countries/v5/codes.alpha_2/${country}`,
+  );
+  request.setRequestHeader(
+    'Authorization',
+    'Bearer rc_live_d324866a6ef74a52a312bbed3af91888',
+  );
+  request.send();
+}
+
+function renderCountry(data) {
+  const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flag.url_svg}"/>
+        <div class="country__data">
+        <h3 class="country__name">${data.names.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)} millions</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].code}</p>
+        </div>
+    </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+
+function getCountryAndNeighbour(country) {
+  const request = new XMLHttpRequest();
+  request.addEventListener('load', function () {
+    const data = JSON.parse(this.responseText).data.objects[0];
+    console.log(data);
+    renderCountry(data);
+
+    // render neighbouring country
+    const neighbour = data.borders?.[0];
+    console.log(neighbour);
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', function () {
+      const data = JSON.parse(this.responseText).data.objects[0];
+      console.log(data);
+      renderCountry(data);
+    });
+    request.open(
+      'GET',
+      `https://api.restcountries.com/countries/v5/codes.alpha_2/${neighbour}`,
+    );
+    request.setRequestHeader(
+      'Authorization',
+      'Bearer rc_live_d324866a6ef74a52a312bbed3af91888',
+    );
+    request.send();
   });
   request.open(
     'GET',
@@ -37,9 +109,10 @@ function getCountryData(country) {
   request.send();
 }
 
-getCountryData('PT');
-getCountryData('US');
-getCountryData('DE');
+// getCountryData('PT');
+// getCountryData('US');
+// getCountryData('DE');
+getCountryAndNeighbour('PT');
 
 // NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
